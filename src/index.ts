18881e396ae5fa2bca60c9b48b9ce26aed04c5e9
Maxim Spirkin <maksim.spirkin@sunmait.tech>
2025-09-28 21:28:05 +0300

    feat: enable cors

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,15 @@
 import express from "express";
 import { router } from "./routes";
 import { telegramService } from "./services/telegram.service";
+import cors from "cors";
 
 const app = express();
 
 telegramService.init();
 
 app.use(express.json());
+app.use(cors({ origin: "*" }));
+
 app.use(router);
 
 app.listen(3000, () => {
