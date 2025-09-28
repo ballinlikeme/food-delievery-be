8da1c5b287e85f98f653e65ee53512df55448e20
Maxim Spirkin <maksim.spirkin@sunmait.tech>
2025-09-28 20:21:10 +0300

    feat: netlify deploy

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,10 @@
 import express from "express";
-import { telegramService } from "./services/telegram.service";
 import { router } from "./routes";
 
-const bootstrap = async () => {
-    const app = express();
+const app = express();
 
-    app.use(router);
+app.use(router);
 
-    app.listen(3000, () => {
-        telegramService.init();
-        console.log("Server is live");
-    });
-};
-
-bootstrap();
+app.listen(3000, () => {
+    console.log("Server is live");
+});
